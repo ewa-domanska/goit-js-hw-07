2009f813d63e9a21a44b5805b61f134e842f9160
Ewa Domańska <ewadomanska0510@gmail.com>
2022-05-31 23:22:19 +0200

    part of 1 task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,20 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+let html = "";
+let gallery = document.querySelector(".gallery")
+for (const galleryItem of galleryItems) {
+    console.log(galleryItem);
+    html = html + `<div class="gallery__item">
+  <a class="gallery__link" href="">
+    <img
+      class="gallery__image"
+      src="small-image.jpg"
+      data-source="large-image.jpg"
+      alt="Image description"
+    />
+  </a>
+</div>`
+}
+
+gallery.innerHTML =
