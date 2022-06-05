9a9107377642b714c5f18883578de7100b972f13
Ewa Domańska <ewadomanska0510@gmail.com>
2022-06-05 19:48:46 +0200

    1.2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,4 @@
-import { galleryItems } from './gallery-items.js';
+import {galleryItems} from './gallery-items.js';
 // Change code below this line
 
 let html = "";
@@ -17,3 +17,12 @@ for (const galleryItem of galleryItems) {
 }
 
 gallery.innerHTML = html;
+
+function showBigPicture(e) {
+    e.preventDefault();
+    let image = e.target;
+    console.log(image.dataset.source)
+
+}
+
+gallery.addEventListener("click", showBigPicture)
