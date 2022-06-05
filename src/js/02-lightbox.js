4f5cecb1c45401c37696c22c7fb99242e88cb3bc
Ewa Domańska <ewadomanska0510@gmail.com>
2022-06-05 20:39:44 +0200

    2.1

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,12 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+let html = "";
+let gallery = document.querySelector(".gallery")
+for (const galleryItem of galleryItems) {
+    html = html + `<a class="gallery__item" href="${galleryItem.original}">
+  <img class="gallery__image" src="${galleryItem.preview}" alt="${galleryItem.description}" />
+</a>`
+}
+
+gallery.innerHTML = html;
